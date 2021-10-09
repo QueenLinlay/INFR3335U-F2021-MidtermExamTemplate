769963e58bd868778c82dcb1c904c8252d8edcf4
QueenLinlay <leejusti217@gmail.com>
2021-10-09 06:09:05 -0400

    New changes - added more models
    Upgrade some code and added more models - not final

--- Assets/Scripts/DeleteObject.cs
+++ Assets/Scripts/DeleteObject.cs
@@ -5,6 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class DeleteObject : MonoBehaviour
 {
+    public float coinZ = 1;
     public static int counter = 0;
     // Start is called before the first frame update
     void Start()
@@ -13,6 +14,11 @@ public class DeleteObject : MonoBehaviour
     }
 
     // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0.0f, 0.0f, coinZ);
+    }
+
     void OnTriggerEnter(){
         if (counter == 9)
         {
--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -6,23 +6,28 @@ using UnityEngine.SceneManagement;
 public class Movement : MonoBehaviour
 {
     Rigidbody rigidbody;
+    Vector3 RotateObject;
     public float speed = 10.0f;
-    public int counter = 0;
+    //public float RSpeed = 10.0f;
+    public float MouseXSpeed = 10.0f;
 
 	// Start is called before the first frame update
 	void Start()
     {
 		rigidbody = GetComponent<Rigidbody>();
+        RotateObject = new Vector3(0, 0, 0);
 	}
 
 	// Update is called once per frame
 
-
+    //I have tried to add a fix view of the character, like if camera rotate than object will do so. However got stuck trying to keep object and camera rotating same time
+    //So I tried my best.
 
 	void FixedUpdate()
 	{
         float veloX = Input.GetAxis("Horizontal");
         float veloZ = Input.GetAxis("Vertical");
+        float MouseX = MouseXSpeed * Input.GetAxis("Mouse X");
 
         Vector3 veloInput = new Vector3(veloX, 0, veloZ);
         veloInput = Quaternion.Euler(0, Camera.main.transform.rotation.eulerAngles.y, 0) * veloInput;
@@ -32,23 +37,19 @@ public class Movement : MonoBehaviour
         Debug.Log(rigidbody.transform.position);
         //Debug.Log(veloX);
         //Debug.Log(veloZ);
-
+       
         if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.A) || (Input.GetKey(KeyCode.D)))
         {
             rigidbody.AddForce(veloInput * speed);
+            
         }
+       // transform.LookAt(Camera.main.transform.right);
+      //transform.Rotate(Camera.main.transform.up, -MouseX);
+        //transform.Rotate(Camera.main.transform.right, MouseY);
+        transform.Rotate(0.0f, -MouseX, 0);
         rigidbody.AddForce(veloInput * speed);
-    }
+        rigidbody.useGravity = true;
+        rigidbody.isKinematic = false;
 
-    private void OnCollisionEnter(Collision collision)
-    {
-        if (collision.collider.name == "Coins")
-        {
-            counter = counter + 1;
-        }
-        if (counter == 1)
-        {
-            SceneManager.LoadScene("End");
-        }
     }
 }
